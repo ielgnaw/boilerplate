d4f33a5262b99f46a96d8260c909b95e0dbee855
ielgnaw <wuji0223@gmail.com>
2017-07-03 18:43:34 +0800

    add react all in one boilerplate

--- pure-reactallinone/src/reducers/index.js
+++ pure-reactallinone/src/reducers/index.js
@@ -4,11 +4,14 @@
  */
 
 import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
+
 import counter from './counter';
 import ajaxTest from './ajaxTest';
 
 // 使用 redux 的 combineReducers 方法将所有 reducer 打包起来
 export default combineReducers({
     counter,
-    ajaxTest
+    ajaxTest,
+    routing: routerReducer
 });
